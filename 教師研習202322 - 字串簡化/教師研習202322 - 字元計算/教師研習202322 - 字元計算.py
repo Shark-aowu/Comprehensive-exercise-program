476bff0a04b560fc2726b76a12fbdfa93909ab3d
Shark_aowu <103398404+Shark-aowu@users.noreply.github.com>
2023-02-07 15:15:32 +0800

    Delete 教師研習202322 - 字元計算.py

--- 教師研習202322 - 字串簡化/教師研習202322 - 字元計算/教師研習202322 - 字元計算.py
+++
@@ -1,7 +0,0 @@
-num = input("user_input: ")
-sum = [0]*128
-for i in range(0, len(num), 1):
-    sum[ord(num[i])] += 1
-for i in range(0, 128, 1):
-    if(sum[i] != 0):
-        print(f"{chr(i)}:{sum[i]}  ", end = "")
